11f3c3b7232d85836af8c79ec1a85767d8f1761e
Aalind Singh <singh.aalind@gmail.com>
2016-10-31 18:24:10 +0530

    Update Dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,63 +1,22 @@
-//code for dijsktra
-#include <iostream>
-#include <climits>
-using namespace std;
-const int numberVertex = 10;
-int minDistance(int dist[], bool ShortestPathTree[])
-{
-	int min = INT_MAX, min_index;
-	for(int ver=0;ver<numberVertex;ver++)
-	{
-		if(ShortestPathTree[ver]==false && dist[ver]<=min)
-		{
-			min=dist[ver];
-			min_index=ver;
-		}
-	}
-	return min_index;
+//short and smooth
+struct edge { int to, length; };
+    
+int dijkstra(const vector< vector<edge> > &graph, int source, int target) {
+    vector<int> min_distance( graph.size(), INT_MAX );
+    min_distance[ source ] = 0;
+    set< pair<int,int> > active_vertices;
+    active_vertices.insert( {0,source} );
+        
+    while (!active_vertices.empty()) {
+        int where = active_vertices.begin()->second;
+        if (where == target) return min_distance[where];
+        active_vertices.erase( active_vertices.begin() );
+        for (auto ed : graph[where]) 
+            if (min_distance[ed.to] > min_distance[where] + ed.length) {
+                active_vertices.erase( { min_distance[ed.to], ed.to } );
+                min_distance[ed.to] = min_distance[where] + ed.length;
+                active_vertices.insert( { min_distance[ed.to], ed.to } );
+            }
+    }
+    return INT_MAX;
 }
-void dijkstra(int graph[numberVertex][numberVertex], int source)
-{
-	int dist[numberVertex];
-	bool ShortestPathTree[numberVertex];
-	for(int i=0; i<numberVertex;i++)
-	{
-		dist[i]=INT_MAX;
-		ShortestPathTree[i]=false;
-	}
-	dist[source] = 0;
-	for(int count=0; count<numberVertex-1; count++)
-	{
-		int u=minDistance(dist, ShortestPathTree);
-		ShortestPathTree[u]=true;
-		for(int ver = 0; ver < numberVertex; ver++)
-		{
-			if(ShortestPathTree[ver] == 0 && graph[u][ver] != 0 &&
-					dist[u] != INT_MAX && dist[u] + graph[u][ver] < dist[ver])
-		dist[ver] = dist[u] + graph[u][ver];
-		}
-	}
-	cout << "Distance from Source:" << endl;
-	cout << "Vertex\t\tDistance" << endl;
-	for(int i = 0; i < numberVertex; i++)
-	{
-		cout << i << "\t\t" << dist[i] << endl;
-	}
-}
-
-int main()
-{
-	int graph[numberVertex][numberVertex] = {{0, 14, 0, 7, 0, 0, 0, 8, 0, 10},
-		{14, 0, 8, 0, 0, 0, 0, 11, 0, 0},
-		{0, 8, 0, 7, 0, 4, 0, 0, 2, 0},
-		{7, 0, 7, 0, 9, 12, 0, 0, 0, 5},
-		{0, 0, 0, 9, 0, 0, 0, 0, 0, 0},
-		{0, 0, 4, 0, 0, 0, 2, 0, 0, 11},
-		{0, 0, 0, 12, 0, 2, 0, 1, 6, 15},
-		{8, 11, 0, 0, 0, 0, 1, 0, 7, 0},
-		{0, 0, 2, 0, 0, 0, 6, 7, 0, 0},
-		{10, 0, 0, 5, 0, 11, 15, 0, 0, 0}};
-	dijkstra(graph, 0);
-	return 0;
-}
-
